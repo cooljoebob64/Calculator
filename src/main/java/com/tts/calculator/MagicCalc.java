42343b500ab77911f042bbd87ff741e68041ca4b
Joshua Luppes <cooljoebob64@gmail.com>
2021-01-14 14:20:06 -0600

    Implemented calculator functions

---
+++ src/main/java/com/tts/calculator/MagicCalc.java
@@ -0,0 +1,10 @@
+package com.tts.calculator;
+
+public class MagicCalc extends Calculator{
+
+    private void squareRoot(int num){}
+    private void sin(int num){}
+    private void cosine(int num){}
+    private void tangent(){}
+    private void factorial(){}
+}
